80dc1b84ca25dc3c646a771011d5350e016c4672
Luis Cáceres <lacaceres97@gmail.com>
2020-12-27 02:29:42 +0000

    Reformatting of iniconfig.{h,cpp}

--- kernel32/iniconfig.cpp
+++ kernel32/iniconfig.cpp
@@ -8,323 +8,304 @@
 
 #include "utils.h"
 
-template<typename TK, typename TV>
+template <typename TK, typename TV>
 std::vector<TK> extract_keys(std::map<TK, TV> const& input_map) {
-    std::vector<TK> retval;
-    retval.reserve(input_map.size());
+  std::vector<TK> retval;
+  retval.reserve(input_map.size());
 
-    for(const auto& element : input_map)
-        retval.push_back(element.first);
+  for(const auto& element : input_map)
+    retval.push_back(element.first);
 
-    return retval;
+  return retval;
 }
 
-template<typename TK, typename TV>
+template <typename TK, typename TV>
 std::vector<TV> extract_values(std::map<TK, TV> const& input_map) {
-    std::vector<TV> retval;
-    retval.reserve(input_map.size());
+  std::vector<TV> retval;
+  retval.reserve(input_map.size());
 
-    for(const auto& element : input_map)
-        retval.push_back(element.second);
+  for(const auto& element : input_map)
+    retval.push_back(element.second);
 
-    return retval;
+  return retval;
 }
 
-INIConfiguration::Section::Section(std::string  sName) : m_Name(std::move(sName)), m_PropertyMap() {
-}
+INIConfiguration::Section::Section(std::string sName) : m_Name(std::move(sName)), m_PropertyMap() {}
 
 std::optional<std::string> INIConfiguration::Section::getStringProperty(const std::string& name) const {
-    try {
-        return m_PropertyMap.at(str::to_lower(name)).m_Value;
-    } catch(std::out_of_range& oorexcept) {
-        return std::nullopt;
-    }
+  try {
+    return m_PropertyMap.at(str::to_lower(name)).m_Value;
+  } catch(std::out_of_range& oorexcept) { return std::nullopt; }
 }
 
-std::optional<std::vector<std::string>>
-INIConfiguration::Section::getStringListProperty(const std::string& name) const {
-    try {
-        std::string_view propVal = m_PropertyMap.at(str::to_lower(name)).m_Value;
+std::optional<std::vector<std::string>> INIConfiguration::Section::getStringListProperty(const std::string& name) const {
+  try {
+    std::string_view propVal = m_PropertyMap.at(str::to_lower(name)).m_Value;
 
-        std::vector<std::string> list = str::tokenize(propVal, ",");
-        std::for_each(list.begin(), list.end(), [](std::string& s) {
-          s = str::view::trim(s);
-        });
+    std::vector<std::string> list = str::tokenize(propVal, ",");
+    std::for_each(list.begin(), list.end(), [](std::string& s) { s = str::view::trim(s); });
 
-        return list;
-    } catch(std::out_of_range& oorexcept) {
-        return std::nullopt;
-    }
+    return list;
+  } catch(std::out_of_range& oorexcept) { return std::nullopt; }
 }
 
 std::optional<int32_t> INIConfiguration::Section::getIntProperty(const std::string& name) const {
-    try {
-        return boost::lexical_cast<int32_t>(m_PropertyMap.at(str::to_lower(name)).m_Value);
-    } catch(std::out_of_range& oorexcept) {
-        return std::nullopt;
-    }
+  try {
+    return boost::lexical_cast<int32_t>(m_PropertyMap.at(str::to_lower(name)).m_Value);
+  } catch(std::out_of_range& oorexcept) { return std::nullopt; }
 }
 
 std::optional<double> INIConfiguration::Section::getFloatProperty(const std::string& name) const {
-    try {
-        return boost::lexical_cast<double>(m_PropertyMap.at(str::to_lower(name)).m_Value);
-    } catch(std::out_of_range& oorexcept) {
-        return std::nullopt;
-    }
+  try {
+    return boost::lexical_cast<double>(m_PropertyMap.at(str::to_lower(name)).m_Value);
+  } catch(std::out_of_range& oorexcept) { return std::nullopt; }
 }
 
 std::vector<std::string> INIConfiguration::Section::getEntries() const {
-    std::vector<std::string> entries;
-    entries.reserve(m_PropertyMap.size());
+  std::vector<std::string> entries;
+  entries.reserve(m_PropertyMap.size());
 
-    for(const auto& entry : m_PropertyMap) {
-        std::stringstream ss;
-        ss << entry.first << "=" << entry.second.m_Value;
-        entries.push_back(ss.str());
-    }
+  for(const auto& entry : m_PropertyMap) {
+    std::stringstream ss;
+    ss << entry.first << "=" << entry.second.m_Value;
+    entries.push_back(ss.str());
+  }
 
-    return entries;
+  return entries;
 }
 
 std::vector<std::string> INIConfiguration::Section::getKeyNames() const {
-    return extract_keys(m_PropertyMap);
+  return extract_keys(m_PropertyMap);
 }
 
 bool INIConfiguration::load(const std::filesystem::path& filepath) {
-    std::ifstream fis(filepath);
+  std::ifstream fis(filepath);
 
-    return load(fis);
+  return load(fis);
 }
 
 bool INIConfiguration::load(std::istream& inStream) {
-    if(!inStream.good())
-        return false;
+  if(!inStream.good())
+    return false;
 
-    std::string currSectionName;
+  std::string currSectionName;
 
-    std::string line;
-    std::getline(inStream, line);
+  std::string line;
+  std::getline(inStream, line);
 
-    while(!inStream.eof() && !inStream.bad()) {
-        if(line[0] == '[') {
-            currSectionName = line.substr(1, line.find_last_of(']') - 1);
-            addSection(currSectionName);
-        } else if(line[0] != '#' && line.length() > 2) {
-            unsigned long crloc = line.length() - 1;
+  while(!inStream.eof() && !inStream.bad()) {
+    if(line[0] == '[') {
+      currSectionName = line.substr(1, line.find_last_of(']') - 1);
+      addSection(currSectionName);
+    } else if(line[0] != '#' && line.length() > 2) {
+      unsigned long crloc = line.length() - 1;
 
-            if(crloc >= 0 && line[crloc] == '\r') //check for Windows-style newline
-                line = line.substr(0, crloc); //and correct
+      if(crloc >= 0 && line[crloc] == '\r')    //check for Windows-style newline
+        line = line.substr(0, crloc);          //and correct
 
-            std::string key, val;
+      std::string key, val;
 
-            if(parseProperty(line, key, val))
-                addProperty(currSectionName, key, val);
-        }
-
-        std::getline(inStream, line);
+      if(parseProperty(line, key, val))
+        addProperty(currSectionName, key, val);
     }
 
-    return !inStream.bad();
+    std::getline(inStream, line);
+  }
+
+  return !inStream.bad();
 }
 
 bool INIConfiguration::save(const std::filesystem::path& filepath) const {
-    std::ofstream os(filepath);
+  std::ofstream os(filepath);
 
-    return save(os);
+  return save(os);
 }
 
 bool INIConfiguration::save(std::ostream& outStream) const {
-    if(!outStream.good())
-        return false;
+  if(!outStream.good())
+    return false;
 
-    std::for_each(m_SectionMap.begin(), m_SectionMap.end(), [&](const std::pair<std::string, Section>& selem) {
-        if(!selem.second.m_Name.empty())
-            outStream << "[" << selem.second.m_Name << "]" << std::endl;
+  std::for_each(m_SectionMap.begin(), m_SectionMap.end(), [&](const std::pair<std::string, Section>& selem) {
+    if(!selem.second.m_Name.empty())
+      outStream << "[" << selem.second.m_Name << "]" << std::endl;
 
-        std::for_each(selem.second.m_PropertyMap.begin(), selem.second.m_PropertyMap.end(),
-                      [&](const Section::property_map::value_type& pelem) {
-                          outStream << pelem.second.m_Name << "=" << pelem.second.m_Value << std::endl;
-                      });
+    std::for_each(selem.second.m_PropertyMap.begin(), selem.second.m_PropertyMap.end(),
+                  [&](const Section::property_map::value_type& pelem) { outStream << pelem.second.m_Name << "=" << pelem.second.m_Value << std::endl; });
 
-        outStream << std::endl;
-    });
+    outStream << std::endl;
+  });
 
-    return true;
+  return true;
 }
 
 void INIConfiguration::clearSection(const std::string& sName) {
-    m_SectionMap.erase(sName);
+  m_SectionMap.erase(sName);
 }
 
-std::string
-INIConfiguration::getStringProperty(const std::string& sName, const std::string& name, const std::string& def) const {
-    auto sectionOptional = getSection(sName);
+std::string INIConfiguration::getStringProperty(const std::string& sName, const std::string& name, const std::string& def) const {
+  auto sectionOptional = getSection(sName);
 
-    if(!sectionOptional)
-        return def;
+  if(!sectionOptional)
+    return def;
 
-    const Section& section = *sectionOptional;
+  const Section& section = *sectionOptional;
 
-    auto propOptional = section.getStringProperty(name);
+  auto propOptional = section.getStringProperty(name);
 
-    if(!propOptional)
-        return def;
+  if(!propOptional)
+    return def;
 
-    return (*propOptional);
+  return (*propOptional);
 }
 
-std::vector<std::string> INIConfiguration::getStringListProperty(const std::string& sName, const std::string& name,
-                                                                 const std::vector<std::string>& def) const {
-    auto sectionOptional = getSection(sName);
+std::vector<std::string> INIConfiguration::getStringListProperty(const std::string& sName, const std::string& name, const std::vector<std::string>& def) const {
+  auto sectionOptional = getSection(sName);
 
-    if(!sectionOptional)
-        return def;
+  if(!sectionOptional)
+    return def;
 
-    const Section& section = *sectionOptional;
+  const Section& section = *sectionOptional;
 
-    auto propOptional = section.getStringListProperty(name);
+  auto propOptional = section.getStringListProperty(name);
 
-    if(!propOptional)
-        return def;
+  if(!propOptional)
+    return def;
 
-    return (*propOptional);
+  return (*propOptional);
 }
 
 int32_t INIConfiguration::getIntProperty(const std::string& sName, const std::string& name, int32_t def) const {
-    auto sectionOptional = getSection(sName);
+  auto sectionOptional = getSection(sName);
 
-    if(!sectionOptional)
-        return def;
+  if(!sectionOptional)
+    return def;
 
-    const Section& section = *sectionOptional;
+  const Section& section = *sectionOptional;
 
-    auto propOptional = section.getIntProperty(name);
+  auto propOptional = section.getIntProperty(name);
 
-    if(!propOptional)
-        return def;
+  if(!propOptional)
+    return def;
 
-    return (*propOptional);
+  return (*propOptional);
 }
 
 double INIConfiguration::getFloatProperty(const std::string& sName, const std::string& name, double def) const {
-    auto sectionOptional = getSection(sName);
+  auto sectionOptional = getSection(sName);
 
-    if(!sectionOptional)
-        return def;
+  if(!sectionOptional)
+    return def;
 
-    const Section& section = *sectionOptional;
+  const Section& section = *sectionOptional;
 
-    auto propOptional = section.getFloatProperty(name);
+  auto propOptional = section.getFloatProperty(name);
 
-    if(!propOptional)
-        return def;
+  if(!propOptional)
+    return def;
 
-    return (*propOptional);
+  return (*propOptional);
 }
 
 void INIConfiguration::addSection(const std::string& sName) {
-    if(!getSection(str::to_lower(sName)))
-        m_SectionMap.emplace(str::to_lower(sName), Section(sName));
+  if(!getSection(str::to_lower(sName)))
+    m_SectionMap.emplace(str::to_lower(sName), Section(sName));
 }
 
 void INIConfiguration::addRawProperty(const std::string& sName, const std::string& raw) {
-    std::string key, val;
+  std::string key, val;
 
-    if(parseProperty(raw, key, val))
-        addProperty(sName, key, val);
+  if(parseProperty(raw, key, val))
+    addProperty(sName, key, val);
 }
 
 void INIConfiguration::setStringProperty(const std::string& sName, const std::string& name, const std::string& val) {
-    if(!getSection(sName))
-        addSection(sName);
+  if(!getSection(sName))
+    addSection(sName);
 
-    addProperty(sName, name, val);
+  addProperty(sName, name, val);
 }
 
-void INIConfiguration::setStringListProperty(const std::string& sName, const std::string& name,
-                                             const std::vector<std::string>& val) {
-    if(!getSection(sName))
-        addSection(sName);
+void INIConfiguration::setStringListProperty(const std::string& sName, const std::string& name, const std::vector<std::string>& val) {
+  if(!getSection(sName))
+    addSection(sName);
 
-    std::stringstream ss;
+  std::stringstream ss;
 
-    for(auto it = val.begin(); it != val.end(); it++) {
-        if(it != val.begin())
-            ss << ",";
+  for(auto it = val.begin(); it != val.end(); it++) {
+    if(it != val.begin())
+      ss << ",";
 
-        ss << (*it);
-    }
+    ss << (*it);
+  }
 
-    addProperty(sName, name, ss.str());
+  addProperty(sName, name, ss.str());
 }
 
 void INIConfiguration::setIntProperty(const std::string& sName, const std::string& name, int32_t val) {
-    if(!getSection(sName))
-        addSection(sName);
+  if(!getSection(sName))
+    addSection(sName);
 
-    addProperty(sName, name, std::to_string(val));
+  addProperty(sName, name, std::to_string(val));
 }
 
 void INIConfiguration::setFloatProperty(const std::string& sName, const std::string& name, double val) {
-    if(!getSection(sName))
-        addSection(sName);
+  if(!getSection(sName))
+    addSection(sName);
 
-    addProperty(sName, name, std::to_string(val));
+  addProperty(sName, name, std::to_string(val));
 }
 
 std::vector<std::string> INIConfiguration::getSectionNames() const {
-    return extract_keys(m_SectionMap);
+  return extract_keys(m_SectionMap);
 }
 
 std::vector<std::string> INIConfiguration::getEntries(const std::string& sName) const {
-    auto sectionOptional = getSection(sName);
+  auto sectionOptional = getSection(sName);
 
-    if(!sectionOptional)
-        return std::vector<std::string>();
+  if(!sectionOptional)
+    return std::vector<std::string>();
 
-    return sectionOptional->get().getEntries();
+  return sectionOptional->get().getEntries();
 }
 
 std::vector<std::string> INIConfiguration::getKeyNames(const std::string& sName) const {
-    auto sectionOptional = getSection(sName);
+  auto sectionOptional = getSection(sName);
 
-    if(!sectionOptional)
-        return std::vector<std::string>();
+  if(!sectionOptional)
+    return std::vector<std::string>();
 
-    return sectionOptional->get().getKeyNames();
+  return sectionOptional->get().getKeyNames();
 }
 
 bool INIConfiguration::parseProperty(const std::string& raw, std::string& key, std::string& val) {
-    size_t equalsPos = raw.find_first_of('=');
+  size_t equalsPos = raw.find_first_of('=');
 
-    if(equalsPos != std::string::npos) {
-        key = raw.substr(0, equalsPos);
-        val = raw.substr(equalsPos + 1);
+  if(equalsPos != std::string::npos) {
+    key = raw.substr(0, equalsPos);
+    val = raw.substr(equalsPos + 1);
 
-        if(val[0] == '"' || val[0] == '\'')
-            val = val.substr(1);
+    if(val[0] == '"' || val[0] == '\'')
+      val = val.substr(1);
 
-        if(val[val.length() - 1] == '"' || val[val.length() - 1] == '\'')
-            val.resize(val.length() - 1);
+    if(val[val.length() - 1] == '"' || val[val.length() - 1] == '\'')
+      val.resize(val.length() - 1);
 
-        return true;
-    }
+    return true;
+  }
 
-    return false;
+  return false;
 }
 
 void INIConfiguration::addProperty(const std::string& sName, const std::string& name, const std::string& val) {
-    Section::Property p;
-    p.m_Name = str::view::trim(name);
-    p.m_Value = str::view::trim(val);
+  Section::Property p;
+  p.m_Name = str::view::trim(name);
+  p.m_Value = str::view::trim(val);
 
-    m_SectionMap.at(str::to_lower(sName)).m_PropertyMap[str::to_lower(p.m_Name)] = p;
+  m_SectionMap.at(str::to_lower(sName)).m_PropertyMap[str::to_lower(p.m_Name)] = p;
 }
 
 std::optional<std::reference_wrapper<const INIConfiguration::Section>> INIConfiguration::getSection(const std::string& sName) const {
-    try {
-        return m_SectionMap.at(str::to_lower(sName));
-    } catch(std::out_of_range& ex) {
-        return std::nullopt;
-    }
+  try {
+    return m_SectionMap.at(str::to_lower(sName));
+  } catch(std::out_of_range& ex) { return std::nullopt; }
 }
--- kernel32/iniconfig.h
+++ kernel32/iniconfig.h
@@ -8,93 +8,77 @@
 #include <vector>
 
 class INIConfiguration {
-        friend class INIParser;
+  friend class INIParser;
 
-        class Section {
-                friend class INIConfiguration;
+  class Section {
+    friend class INIConfiguration;
 
-                friend class INIParser;
+    friend class INIParser;
 
-                struct Property {
-                    std::string m_Name;
-                    std::string m_Value;
-                };
+    struct Property {
+      std::string m_Name;
+      std::string m_Value;
+    };
 
-                typedef std::map<std::string, Property> property_map;
-            public:
-                Section(const Section& s) = default;
+    typedef std::map<std::string, Property> property_map;
 
-                Section(Section&& s) = default;
+   public:
+    Section(const Section& s) = default;
+    Section(Section&& s) = default;
 
-                std::optional<std::string> getStringProperty(const std::string& name) const;
+    std::optional<std::string> getStringProperty(const std::string& name) const;
+    std::optional<std::vector<std::string>> getStringListProperty(const std::string& name) const;
+    std::optional<int32_t> getIntProperty(const std::string& name) const;
+    std::optional<double> getFloatProperty(const std::string& name) const;
 
-                std::optional<std::vector<std::string>> getStringListProperty(const std::string& name) const;
+    std::vector<std::string> getEntries() const;
+    std::vector<std::string> getKeyNames() const;
 
-                std::optional<int32_t> getIntProperty(const std::string& name) const;
+   private:
+    explicit Section(std::string sname);
 
-                std::optional<double> getFloatProperty(const std::string& name) const;
+    std::string m_Name;
+    property_map m_PropertyMap;
+  };
 
-                std::vector<std::string> getEntries() const;
+  typedef std::map<std::string, Section> section_map;
 
-                std::vector<std::string> getKeyNames() const;
+ public:
+  bool load(const std::filesystem::path& filepath);
+  bool load(std::istream& inStream);
 
-            private:
-                explicit Section(std::string  sname);
+  bool save(const std::filesystem::path& filepath) const;
+  bool save(std::ostream& outStream) const;
 
-                std::string m_Name;
-                property_map m_PropertyMap;
-        };
+  void clearSection(const std::string& sname);
 
-        typedef std::map<std::string, Section> section_map;
-    public:
-        bool load(const std::filesystem::path& filepath);
+  std::string getStringProperty(const std::string& sname, const std::string& name, const std::string& def = "") const;
+  std::vector<std::string> getStringListProperty(const std::string& sName, const std::string& name,
+                                                 const std::vector<std::string>& def = std::vector<std::string>()) const;
+  int32_t getIntProperty(const std::string& sName, const std::string& name, int32_t def = 0) const;
+  double getFloatProperty(const std::string& sName, const std::string& name, double def = 0.0) const;
 
-        bool load(std::istream& inStream);
+  void addSection(const std::string& sname);
+  void addRawProperty(const std::string& sname, const std::string& raw);
 
-        bool save(const std::filesystem::path& filepath) const;
+  void setStringProperty(const std::string& sname, const std::string& name, const std::string& val);
+  void setStringListProperty(const std::string& sname, const std::string& name, const std::vector<std::string>& val);
+  void setIntProperty(const std::string& sname, const std::string& name, int32_t val);
+  void setFloatProperty(const std::string& sname, const std::string& name, double val);
 
-        bool save(std::ostream& outStream) const;
+  std::vector<std::string> getSectionNames() const;
+  std::vector<std::string> getEntries(const std::string& sname) const;
+  std::vector<std::string> getKeyNames(const std::string& sname) const;
 
-        void clearSection(const std::string& sname);
+ protected:
+  static bool parseProperty(const std::string& raw, std::string& key, std::string& val);
 
-        std::string
-        getStringProperty(const std::string& sname, const std::string& name, const std::string& def = "") const;
+  void addProperty(const std::string& sname, const std::string& name, const std::string& val);
 
-        std::vector<std::string> getStringListProperty(const std::string& sName, const std::string& name,
-                                                       const std::vector<std::string>& def = std::vector<std::string>()) const;
+  std::optional<std::reference_wrapper<const Section>> getSection(const std::string& sname) const;
 
-        int32_t getIntProperty(const std::string& sName, const std::string& name, int32_t def = 0) const;
-
-        double getFloatProperty(const std::string& sName, const std::string& name, double def = 0.0) const;
-
-        void addSection(const std::string& sname);
-
-        void addRawProperty(const std::string& sname, const std::string& raw);
-
-        void setStringProperty(const std::string& sname, const std::string& name, const std::string& val);
-
-        void
-        setStringListProperty(const std::string& sname, const std::string& name, const std::vector<std::string>& val);
-
-        void setIntProperty(const std::string& sname, const std::string& name, int32_t val);
-
-        void setFloatProperty(const std::string& sname, const std::string& name, double val);
-
-        std::vector<std::string> getSectionNames() const;
-
-        std::vector<std::string> getEntries(const std::string& sname) const;
-
-        std::vector<std::string> getKeyNames(const std::string& sname) const;
-
-    protected:
-        static bool parseProperty(const std::string& raw, std::string& key, std::string& val);
-
-        void addProperty(const std::string& sname, const std::string& name, const std::string& val);
-
-        std::optional<std::reference_wrapper<const Section>> getSection(const std::string& sname) const;
-
-    private:
-        section_map m_SectionMap;
+ private:
+  section_map m_SectionMap;
 };
 
-#endif // CONFIGURATION_H
+#endif    // CONFIGURATION_H
