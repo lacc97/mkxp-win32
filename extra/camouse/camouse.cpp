adfeb27e39b2012021b5b3b9e656da3f67338bbe
Luis Cáceres <lacaceres97@gmail.com>
2020-12-27 19:22:54 +0000

    Add tracing on camouse functions

--- extra/camouse/camouse.cpp
+++ extra/camouse/camouse.cpp
@@ -1,20 +1,30 @@
 #include "camouse.hpp"
 
+#include "log.h"
+
 #include "user32.h"
 #include "mkxpGlue.h"
 
 HWND camouse_GetWindowHandle() {
+  SPDLOG_TRACE("camouse::GetWindowHandle()");
+
   return user32_FindWindowA("RGSS Player", nullptr);
 }
 
 LONG camouse_GetWheelDelta() {
+  SPDLOG_TRACE("camouse::GetWheelDelta()");
+
   return 0;
 }
 
 LONG camouse_DisposeHook() {
+  SPDLOG_TRACE("camouse::DisposeHook()");
+
   return 0;
 }
 
 LONG camouse_RegenerationHook() {
+  SPDLOG_TRACE("camouse::RegenerationHook()");
+
   return 0;
 }
